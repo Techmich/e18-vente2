76feb20e3f66435b6a2a322c49fff9a922746bbb
Michel Gaudreau <michel.gaudreau.1@ens.etsmtl.ca>
2018-06-18 02:20:11 -0400

    new branch

---
+++ migrations-db.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'dbname' => 'heroku_a69a2dc30a95123',
+    'user' => 'b3982458fa493b',
+    'password' => 'ef2ba817',
+    'host' => 'us-cdbr-iron-east-04.cleardb.net',
+    'driver' => 'pdo_mysql',
+];
